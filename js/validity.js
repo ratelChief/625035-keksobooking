55aed36c2ebaedc91c83b096b79a39edeb9b6732
ratelChief <dima1002010@live.com>
2018-04-24 16:42:46 +0300

    module4-task2

--- js/validity.js
+++ js/validity.js
@@ -4,20 +4,25 @@
   var type = document.querySelector('#type');
   var price = document.querySelector('#price');
 
-  var getPrice = function (priceForNight) {
+  var setupPrice = function (priceForNight) {
     price.min = priceForNight;
     price.placeholder = price.min;
   };
 
   var onTypeChange = function () {
-    if (type.value === 'flat') {
-      getPrice('1000');
-    } else if (type.value === 'bungalo') {
-      getPrice('0');
-    } else if (type.value === 'house') {
-      getPrice('5000');
-    } else {
-      getPrice('10000');
+    switch (type.value) {
+      case 'flat':
+        setupPrice('1000');
+        break;
+      case 'bungalo':
+        setupPrice('0');
+        break;
+      case 'house':
+        setupPrice('5000');
+        break;
+      case 'palace':
+        setupPrice('10000');
+        break;
     }
   };
 
@@ -28,46 +33,44 @@
   var timeIn = document.querySelector('#timein');
   var timeOut = document.querySelector('#timeout');
 
-  var onTimeInChange = function (evt) {
+  var onCheckInTimeChage = function (evt) {
     var indexIn = evt.currentTarget.selectedIndex;
     timeOut.selectedIndex = indexIn;
   };
 
-  var onTimeOutChange = function (evt) {
+  var onCheckOutTimeChage = function (evt) {
     var indexOut = evt.currentTarget.selectedIndex;
     timeIn.selectedIndex = indexOut;
   };
 
-  timeIn.addEventListener('change', onTimeInChange);
-  timeOut.addEventListener('change', onTimeOutChange);
+  timeIn.addEventListener('change', onCheckInTimeChage);
+  timeOut.addEventListener('change', onCheckOutTimeChage);
 })();
 
 (function () {
   var room = document.querySelector('#room_number');
   var capacity = document.querySelector('#capacity');
-  var capacityOptions = capacity.querySelectorAll('option');
-  for (var i = 1; i < capacityOptions.length; i++) {
-    capacityOptions[i].disabled = true;
-  }
-  var enableOptions = function (index) {
-    i = 0;
-    while (i < index + 1) {
-      capacityOptions[i].disabled = false;
-      i++;
+
+  var initialStateOfOptions = function () {
+    var capacityOptions = capacity.querySelectorAll('option');
+    for (var i = 1; i < capacityOptions.length; i++) {
+      capacityOptions[i].disabled = true;
     }
   };
+  initialStateOfOptions();
 
   var onRoomChange = function (evt) {
+    var roomsCount = Number(evt.target.value);
+    var options = capacity.querySelectorAll('option');
     var index = evt.currentTarget.selectedIndex;
     capacity.selectedIndex = index;
 
-    for (i = 0; i < capacity.length; i++) {
-      capacityOptions[i].disabled = true;
-    }
-    if (index < 3) {
-      enableOptions(index);
-    } else {
-      capacityOptions[index].disabled = false;
+    for (var i = 0; i < options.length; i++) {
+      if (roomsCount === 100) {
+        options[i].disabled = options[i].value > 0;
+      } else {
+        options[i].disabled = options[i].value < 1 || options[i].value > roomsCount;
+      }
     }
   };
 
